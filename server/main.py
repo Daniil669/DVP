39c5615d614deae7d01f3b4ad9bcbae2c0904d61
Daniil Shchennikov <daniil.freeman669@gmail.com>
2025-09-18 15:15:20 +0300

    routes, and main are ready

--- server/main.py
+++ server/main.py
@@ -1,35 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routes.root_node import router as get_root
+from routes.child_node import router as get_child
 
 app = FastAPI()
 
-sample_data=[
-    {"parent_item": "MAT000001", "child_item": "MAT000004", "sequence_no": 1, "level": 1},
-    {"parent_item": "MAT000001", "child_item": "MAT000007", "sequence_no": 10, "level": 1},
-    {"parent_item": "MAT000001", "child_item": "MAT000008", "sequence_no": 11, "level": 1},
-    {"parent_item": "MAT000001", "child_item": "MAT000002", "sequence_no": 2, "level": 1},
-    {"parent_item": "MAT000002", "child_item": "MAT000017", "sequence_no": 1, "level": 2},
-    {"parent_item": "MAT000003", "child_item": "MAT000006", "sequence_no": 1, "level": 2},
-    {"parent_item": "MAT000004", "child_item": "MAT000018", "sequence_no": 1, "level": 2},
-]
+# allowing everything for dev environment
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_headers=["*"],
+    allow_methods=["*"]
+)
 
-def get_node_children(node_id: str):
-    children = []
-    for node in sample_data:
-        if node["parent"] == node_id:
-            children.append(node)
-    return children
-
-def get_root_nodes():
-    roots = []
-    for node in sample_data:
-        if node["parent"] is None:
-            roots.append(node)
-    return roots
-
-@app.get("/")
-def hello():
-    return {"message": "it is working yeahhhhhh"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port = 8000)
+app.include_router(get_root, prefix='/api')
+app.include_router(get_child, prefix='/api')
+    
